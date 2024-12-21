aad08e105d764824ed4fcadbb049c2c49b9c07dc
xunle15 <147711149+xunle15@users.noreply.github.com>
2024-12-21 13:29:05 +0000

    21:28

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -1,5 +1,5 @@
 abstract class Constants {
-  static const appName = "Hiddify";
+  static const appName = "寻乐云";
   static const githubUrl = "https://github.com/hiddify/hiddify-next";
   static const githubReleasesApiUrl =
       "https://api.github.com/repos/hiddify/hiddify-next/releases";
@@ -7,7 +7,7 @@ abstract class Constants {
       "https://github.com/hiddify/hiddify-next/releases/latest";
   static const appCastUrl =
       "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
+  static const telegramChannelUrl = "https://t.me/xunlecloud";
   static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
   static const termsAndConditionsUrl = "https://hiddify.com/terms/";
   static const cfWarpPrivacyPolicy =
